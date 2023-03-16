3178d49bcde2c11024a467715b451913fdd3e523
Nicolas Steven Gutierrez Castiyejo <nicolasgutierrez029@gmail.com>
2023-03-16 12:24:54 -0500

    Mini calculadora terminada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'MiniCalculadoraApp';
+  title = 'Mini Calculadora :)';
+  numero1: number = 0;
+  numero2: number = 0;
+  resultadoSuma: number = 0;
+  resultadoResta: number = 0;
+  resultadoMulti: number = 0;
+  resultadoDiv: number = 0;
+
+  sumar(): void {
+    this.resultadoSuma = this.numero1 + this.numero2;
+  }
+  restar(): void {
+    this.resultadoResta = this.numero1 - this.numero2;
+  }
+  multiplicar(): void {
+    this.resultadoMulti = this.numero1 * this.numero2;
+  }
+  dividir(): void {
+    this.resultadoDiv = this.numero1 / this.numero2;
+  }
 }
